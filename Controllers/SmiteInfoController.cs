47af4dfc9dfd97242e8e33f07d059789fa194925
ItsTheFlea <59777514+ItsTheFlea@users.noreply.github.com>
2020-11-30 15:22:09 -0500

    Added functionality
    Created instances for list of gods and items for use of general listing of content under gods and items page. Created dictionary instances of those lists to help in future refernces to those objects so they can be found by name specifically.

---
+++ Controllers/SmiteInfoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmiteAPIWebsite
+{
+    public class Smite
+    {
+        public static List<Gods> gods = ApiCall.GetGodsInfo();
+
+        public static List<ItemsInfo> items = ApiCall.GetItems();
+
+        public static Dictionary<string, Gods> godsDictionary = ApiCall.GetGodsInfo().ToDictionary(god => god.Name, god => god);
+
+        public static Dictionary<string, ItemsInfo> itemsDictionary = ApiCall.GetItems().ToDictionary(item => item.DeviceName, item => item);
+        
+    }
+}
